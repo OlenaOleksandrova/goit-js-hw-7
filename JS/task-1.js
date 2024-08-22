b1a04edbd1cb6d7d6a926c4ae5004410ece8e605
Olena Oleksandrova <ha240886kev@gmail.com>
2024-08-22 23:12:16 +0300

    js

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,10 +1,12 @@
-const categories = document.querySelectorAll(categories.item);
-console.log(categories.length)
+const categories = document.querySelectorAll(`#categories .item`);
+console.log(`${categories.length}`);
+
+
 
 categories.forEach((category) => {
   
     const title = category.querySelector('h2').textContent;
     const itemAllCaunt = category.querySelector(`li`).length;
     console.log(title);
-    console.log(itemAllCaunt);
+    console.log(`${itemAllCaunt}`);
 });
