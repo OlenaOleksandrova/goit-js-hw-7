198b9a5ac85d9e9416179db9d3847cf97f6e018e
Olena Oleksandrova <ha240886kev@gmail.com>
2024-08-25 23:34:15 +0300

    js

--- JS/task-4.js
+++ JS/task-4.js
@@ -1,22 +1,22 @@
-// 1.2
-const loginNew = document.querySelector('.login-form').addEventListener('submit', (event) => {
-    event.preventDefault();
+// // 1.2
+// const loginNew = document.querySelector('.login-form').addEventListener('submit', (event) => {
+//     event.preventDefault();
 
 
 
-    const { email, password } = event.target.elements;
+//     const { email, password } = event.target.elements;
 
-    const emailEl = email.value.trim();
-    const passwordEl = password.value.trim();
-    if (emailEl === `` || passwordEl === ``) {
-        alert('All form fields must be filled in');
-        return;
-}
-const formData = {
-    email: emailEl,
-    password: passwordEl,
-};
-console.log(formData);
-// loginNew.reset();
-});
+//     const emailEl = email.value.trim();
+//     const passwordEl = password.value.trim();
+//     if (emailEl === `` || passwordEl === ``) {
+//         alert('All form fields must be filled in');
+//         return;
+// }
+// const formData = {
+//     email: emailEl,
+//     password: passwordEl,
+// };
+// console.log(formData);
+// // loginNew.reset();
+// });
 
