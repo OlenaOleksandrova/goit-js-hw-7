996af52f7cfd7f237c75d9bff5b8f4daaac1e58a
Olena Oleksandrova <ha240886kev@gmail.com>
2024-08-29 13:31:52 +0300

    scc

--- JS/task-6.js
+++ JS/task-6.js
@@ -35,10 +35,4 @@ createBtn.addEventListener('click', () => {
 
   destroyBtn.addEventListener('click', createBoxes);
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
 
